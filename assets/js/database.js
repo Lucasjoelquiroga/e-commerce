41ceffbdb3dc3cc855d7d8c818118a77259dab13
Iuriiiii <metralicnetwork@gmail.com>
2022-07-11 12:18:09 -0300

    añadido 1 template y referencia de concepto de diseño

--- assets/js/database.js
+++ assets/js/database.js
@@ -3,6 +3,8 @@ function createProduct(name, url = '', price = 0, description = '', amount = 1)
     return {name: name, url: url, amount: amount, price: price, description: description};
 }
 
+/* Añadir más productos */
+
 export default {
     cars: [
         createProduct('Tesla', '/assets/img/cars/tesla.png', 28000, 'Auto electrico')
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,3 @@
+/* Esta base de datos contiene los elementos a vender */
 import db from './database.js';
 
