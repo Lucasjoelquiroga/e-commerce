d79b1a6074d8054b10e760bb08a86c55f99233bf
Iuriiiii <metralicnetwork@gmail.com>
2022-07-11 21:47:07 -0300

    +productos, +js, +comentarios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,27 @@
 /* Esta base de datos contiene los elementos a vender */
-import db from './database.js';
+import products from './database.js';
 
+/* Elementos */
+let main = document.querySelector('.main');
+let section4products = main.childNodes[1];
+let section4cart = main.childNodes[3];
+
+/* El array dónde se contendrán los elementos que se comprarán */
+let cart = []; 
+
+function drawProducts()
+{
+
+}
+
+function drawCart()
+{
+
+}
+
+/* Utilizado para dibujar tanto los productos cómo los enlistados */
+function drawBoth()
+{
+    drawProducts();
+    drawCart();
+}
