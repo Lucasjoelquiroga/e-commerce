d348d729b99f2e6ac659770e777e5b25666ee902
Iuriiiii <metralicnetwork@gmail.com>
2022-07-12 00:48:06 -0300

    +js +draw de productos finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,54 @@ import products from './database.js';
 let main = document.querySelector('.main');
 let section4products = main.childNodes[1];
 let section4cart = main.childNodes[3];
+let productTemplate = document.getElementsByTagName('template')[0].content;
 
 /* El array dónde se contendrán los elementos que se comprarán */
 let cart = []; 
 
-function drawProducts()
+/* products functions */
+products.getElement = function(i)
 {
+    if(i < 0 || i >= products.length)
+        return null;
+    
+    let product =  products[i];
+    let template = productTemplate.cloneNode(true);
+    
+    let h2 = template.querySelector('h2');
+    let img = template.querySelector('img');
+    let p = template.querySelector('p');
+    
+    h2.innerText = product.name;
+    img.src = product.url;
+    p.innerText = product.description;
 
+    return template;
+};
+
+/* Inserta un producto en un container */
+products.appendTo = function(i, element)
+{
+    if(i < 0 || i >= this.length)
+        return;
+
+    element.appendChild(products.getElement(i));
+};
+
+/* Inserta todos los productos en un container */
+products.drawAll = function(container)
+{
+    products.forEach((_, i) => products.appendTo(i, container));
+};
+
+/* Función genérica para dibujar todos los productos en el section */
+function drawProducts(container)
+{
+    container.innerHTML = '';
+    products.drawAll(container);
 }
 
-function drawCart()
+function drawCart(container)
 {
 
 }
@@ -22,6 +60,8 @@ function drawCart()
 /* Utilizado para dibujar tanto los productos cómo los enlistados */
 function drawBoth()
 {
-    drawProducts();
-    drawCart();
+    drawProducts(section4products);
+    drawCart(section4cart);
 }
+
+drawBoth();
